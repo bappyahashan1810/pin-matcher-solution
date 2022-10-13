c3088e0f536630a005516f88dc23e19ada748ef0
bappy-ahashan <bappyahashan@gmail.com>
2022-10-13 13:00:28 +0600

    add calculate input and display

--- js/app.js
+++ js/app.js
@@ -12,4 +12,24 @@ function getPin() {
 function generatePin() {
     const pin = getPin();
     console.log(pin);
+    document.getElementById('display-pin').value = pin;
 }
+
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-number');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+
+
+
+})
